4bdc06c5f075053bd75fc0864cf31c0436a4633c
adrianrl99 <61700204+adrianrl99@users.noreply.github.com>
2021-05-01 12:34:19 -0400

    update

--- lib/src/config.dart
+++ lib/src/config.dart
@@ -1,13 +1,8 @@
 library config.globals;
 
-import 'package:bettycook/src/constants.dart';
 import 'package:bettycook/src/database.dart';
 import 'package:bettycook/src/hive_database.dart';
-import 'package:hive/hive.dart';
 
 // Databases
 RecipesDatabase db = RecipesDatabase();
 HiveDatabase hiveDB = HiveDatabase();
-
-// Hive Boxes
-Box settingsBox = Hive.box(settingsBoxKey);
