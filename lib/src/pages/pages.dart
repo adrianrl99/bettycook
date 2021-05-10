6a6f0933d566312cb52d560bc0f3e1a1bddd9af3
adrianrl99 <61700204+adrianrl99@users.noreply.github.com>
2021-05-09 22:10:29 -0400

    update

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -4,12 +4,7 @@ export "./home_page.dart";
 export './recipe_page.dart';
 export './subcategory_page.dart';
 export './tips_page.dart';
-export './i_want_cook_page.dart';
-export './calendar_page.dart';
-export './to_buy_page.dart';
-export './converter_page.dart';
 export './about_page.dart';
-export './settings_page.dart';
 
 // Search Pages
 export './search/search_all_page.dart';
--- lib/src/pages/recipe_page.dart
+++ lib/src/pages/recipe_page.dart
@@ -1,18 +1,14 @@
 import 'dart:io';
 
-import 'package:badges/badges.dart';
 import 'package:bettycook/src/config.dart';
 import 'package:bettycook/src/widgets/bottom_nav_bar.dart';
 import 'package:bettycook/src/widgets/fullscreen_photo_widget.dart';
-import 'package:bettycook/src/widgets/recipe_page/calendar_tab_widget.dart';
 import 'package:bettycook/src/widgets/recipe_page/ingredients_tab_widget.dart';
-import 'package:bettycook/src/widgets/recipe_page/notes_widget.dart';
 import 'package:bettycook/src/widgets/recipe_page/preparation_tab_widget.dart';
 import 'package:bettycook/src/widgets/recipe_page/title_widget.dart';
 import 'package:bettycookplugins/bettycookplugins.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
-import 'package:hive/hive.dart';
 
 class RecipePage extends StatefulWidget {
   static const routeName = "/recipe";
@@ -31,23 +27,11 @@ class RecipePage extends StatefulWidget {
 class _RecipePageState extends State<RecipePage> {
   ScrollController controller = ScrollController();
 
-  void showNotesDialog(BuildContext context) async {
-    await showDialog(
-        context: context,
-        useSafeArea: true,
-        useRootNavigator: true,
-        builder: (BuildContext context) {
-          return NotesWidget(
-            recipe: this.widget.recipe,
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: DefaultTabController(
-        length: 3,
+        length: 2,
         child: NestedScrollView(
           controller: controller,
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
@@ -71,10 +55,6 @@ class _RecipePageState extends State<RecipePage> {
                             )));
                           });
                     },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {},
                   ),
                 ],
                 centerTitle: true,
@@ -113,9 +93,6 @@ class _RecipePageState extends State<RecipePage> {
                     Tab(
                       child: Text("Preparacion"),
                     ),
-                    Tab(
-                      child: Text("Calendario"),
-                    ),
                   ],
                 ),
               )
@@ -129,29 +106,12 @@ class _RecipePageState extends State<RecipePage> {
               PreparationTabWidget(
                   preparation: this.widget.recipe.preparation,
                   title: this.widget.recipe.title),
-              CalendarTabWidget(recipe: this.widget.recipe),
             ],
           ),
         ),
       ),
       bottomNavigationBar: BottomNavBar(
         recipe: this.widget.recipe,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: ValueListenableBuilder(
-        valueListenable: hiveDB.recipesBoxBaseListable(),
-        builder:
-            (BuildContext context, Box<RecipeHive> recipexBox, Widget? child) {
-          return Badge(
-            position: BadgePosition.bottomStart(),
-            showBadge: this.widget.recipe.notes.isNotEmpty,
-            badgeContent: Text(this.widget.recipe.notes.length.toString()),
-            child: FloatingActionButton(
-              child: Icon(Icons.notes),
-              onPressed: () => showNotesDialog(context),
-            ),
-          );
-        },
       ),
     );
   }
